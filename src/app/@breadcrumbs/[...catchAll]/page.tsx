5c406ad43984a39138f715fe418532bf110ea0ed
ali atwa <ali.atwa2040@gmail.com>
2025-07-04 00:07:16 +0300

    Implement breadcrumbs functionality in SiteHeader and Layout components

---
+++ src/app/@breadcrumbs/[...catchAll]/page.tsx
@@ -0,0 +1,10 @@
+import { Breadcrumbs } from "@/components/breadcrumbs";
+
+type Props = {
+  params: {
+    catchAll: string[];
+  };
+};
+export default function BreadcrumbSlot({ params: { catchAll } }: Props) {
+  return <Breadcrumbs routes={catchAll} />;
+}
