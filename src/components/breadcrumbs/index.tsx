bbb0b964b21f759d9475d8838968bec8b8705add
ali atwa <ali.atwa2040@gmail.com>
2025-07-04 01:08:04 +0300

    Enhance Breadcrumbs component by adding nested route support and improving breadcrumb generation logic

--- src/components/breadcrumbs/index.tsx
+++ src/components/breadcrumbs/index.tsx
@@ -9,7 +9,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export function Breadcrumbs() {
   const pathname = usePathname();
@@ -45,7 +45,30 @@ export function Breadcrumbs() {
       );
     }
   }
+  function generateBreadcrumbs() {
+    // Remove any query parameters, as those aren't included in breadcrumbs
+    const asPathWithoutQuery = pathname.split("?")[0];
 
+    // Break down the path between "/"s, removing empty entities
+    // Ex:"/my/nested/path" --> ["my", "nested", "path"]
+    const asPathNestedRoutes = asPathWithoutQuery
+      .split("/")
+      .filter((v) => v.length > 0);
+
+    // Iterate over the list of nested route parts and build
+    // a "crumb" object for each one.
+    const crumblist = asPathNestedRoutes.map((subpath, idx) => {
+      // We can get the partial nested route for the crumb
+      // by joining together the path parts up to this point.
+      const href = "/" + asPathNestedRoutes.slice(0, idx + 1).join("/");
+      // The title will just be the route string for now
+      const text = subpath;
+      return { href, text };
+    });
+
+    // Add in a default "Home" crumb for the top-level
+    return [{ href: "/", text: "Home" }, ...crumblist];
+  }
   return (
     <Breadcrumb>
       <BreadcrumbList>
@@ -53,7 +76,7 @@ export function Breadcrumbs() {
           <BreadcrumbLink href="/">Home</BreadcrumbLink>
         </BreadcrumbItem>
         {breadcrumbItems}
-        <BreadcrumbSeparator />
+        {breadcrumbItems.length > 0 && <BreadcrumbSeparator />}
         {breadcrumbPage}
       </BreadcrumbList>
     </Breadcrumb>
