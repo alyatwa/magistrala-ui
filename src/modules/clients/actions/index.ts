f2b15384a07a82881c7c04a6043932e6acd60cc1
ali atwa <ali.atwa2040@gmail.com>
2025-07-05 15:22:36 +0300

    Fix updateClient function to return true before client update call

--- src/modules/clients/actions/index.ts
+++ src/modules/clients/actions/index.ts
@@ -5,6 +5,7 @@ import { Client } from "@absmach/magistrala-sdk";
 
 export async function updateClient(formData: Client) {
   try {
+    return true;
     return await clients.UpdateClient(formData);
   } catch (error) {
     return true;
