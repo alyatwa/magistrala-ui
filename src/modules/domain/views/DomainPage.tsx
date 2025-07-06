74f3df8a84295b9f78b7fbee0751c25235049a24
ali atwa <ali.atwa2040@gmail.com>
2025-07-07 00:48:17 +0300

    Fix formatting and update link handling in NavSecondary component

--- src/modules/domain/views/DomainPage.tsx
+++ src/modules/domain/views/DomainPage.tsx
@@ -157,7 +157,7 @@ export const DomainPage = () => {
 
   return (
     <div className="container mx-auto mt-4 pb-4 md:pb-8">
-      <div className="border rounded-md m-2 sm:p-4">
+      <div className="  m-2 sm:p-4">
         <div className="relative w-full overflow-x-auto">
           {domains.map((domain) => (
             <Table key={domain.id} className="w-full caption-bottom text-sm ">
