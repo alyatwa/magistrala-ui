1650fd4ca7cb81e7c80cc9b63dd6bb3d0b8707a3
ali atwa <ali.atwa2040@gmail.com>
2025-07-10 23:46:56 +0300

    refactor(OilRig, LandingPage): remove background color for improved design consistency

--- src/modules/landing/views/LandingPage.tsx
+++ src/modules/landing/views/LandingPage.tsx
@@ -6,7 +6,7 @@ import { Report } from "../components/Report";
 export const LandingPage = () => {
   return (
     <div className="grid w-full items-center justify-items-center min-h-screen ">
-      <main className="flex flex-col gap-4 w-full">
+      <main className="flex flex-col gap-4 w-full bg-green-50/60 ">
         <OilRig />
         <Report />
         <Contact />
