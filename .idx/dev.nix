f1a5a2d648ce93dd107ebed3821875af7276d1c8
ali.atwa2040 <ali.atwa2040@gmail.com>
2025-07-12 07:48:44 +0000

    nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,21 +6,21 @@
   idx.extensions = [
     
   ];
-  idx.previews = {
-    previews = {
-      web = {
-        command = [
-          "npm"
-          "run"
-          "dev"
-          "--"
-          "--port"
-          "$PORT"
-          "--hostname"
-          "0.0.0.0"
-        ];
-        manager = "web";
-      };
-    };
-  };
+  # idx.previews = {
+  #   previews = {
+  #     web = {
+  #       command = [
+  #         "npm"
+  #         "run"
+  #         "dev"
+  #         "--"
+  #         "--port"
+  #         "$PORT"
+  #         "--hostname"
+  #         "0.0.0.0"
+  #       ];
+  #       manager = "web";
+  #     };
+  #   };
+  # };
 }
