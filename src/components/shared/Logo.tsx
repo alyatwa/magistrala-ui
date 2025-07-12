d2dc23cf77d46878ba59e1ce8f255f55f0602c24
ali atwa <ali.atwa2040@gmail.com>
2025-07-12 20:24:17 +0000

    icon

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -9,7 +9,8 @@ export const Logo = ({ className }: LogoProps) => {
   return (
     <div className={cn(`flex items-center space-x-2`, className)}>
       <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
-        <Zap className="w-4 h-4 text-black" />
+        {/* <Zap className="w-4 h-4 text-black" /> */}
+        <img src="/cloud.png" className="w-6 h-6 text-black" />
       </div>
       <span className="text-white font-semibold text-lg">CTX</span>
     </div>
